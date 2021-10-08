dc3051f14f30c38aef1bddbad610f80475d857db
MaxymKucherenko <maxym_88@ukr.net>
2021-10-09 01:23:37 +0300

    new metods added

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
+import { v4 as uuidv4 } from "uuid";
 
 const INITIAL_STATE = {
-  contacts: [],
   name: "",
   number: "",
 };
@@ -18,14 +18,30 @@ class Form extends Component {
 
   handleSubmit = (e) => {
     e.preventDefault();
-    this.props.onSubmit(this.state);
+    const { name, number } = this.state
+    const { onAdd } = this.props
+    const isValidateForm = this.validateForm()
+    if (!isValidateForm) return
+    onAdd({ id: uuidv4(), name, number });
+    
     this.reset();
   };
+
+  validateForm = () => {
+    const { name, number } = this.state;
+    const { onCheckUnique } = this.props;
+    if (!name || !number) {
+      alert('Some field is empty')
+      return false
+    }
+    return onCheckUnique(name);
+  }
 
   reset = () => {
     this.setState({ ...INITIAL_STATE});
   };
   render() {
+    const { name, number} = this.state
     return (
       <form onSubmit={this.handleSubmit}>
         <label>
@@ -33,7 +49,7 @@ class Form extends Component {
           <input
             type="text"
             name="name"
-            value={this.state.name}
+            value={name}
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
             onChange={this.handleInputChange}
@@ -45,7 +61,7 @@ class Form extends Component {
           <input
             type="tel"
             name="number"
-            value={this.state.number}
+            value={number}
             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
             title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
             onChange={this.handleInputChange}
@@ -53,7 +69,7 @@ class Form extends Component {
           />
         </label>
 
-        <button type="submit" /* onClick={GoodIncrement} */>Add contact</button>
+        <button type="submit">Add contact</button>
       </form>
     );
   }
